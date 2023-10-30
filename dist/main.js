16294bfa8b1d73135b9362ae76bf9b7c05b1ec10
ebeshero <ebbondar@gmail.com>
2023-10-30 15:55:51 -0400

    trying simpler import line for three.js

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.skypack.dev/three';
+import THREE from 'https://cdn.skypack.dev/three';
 //import './style.css';
 /*import javascriptLogo from './javascript.svg'
 // import viteLogo from 'public.vite.svg'*/
